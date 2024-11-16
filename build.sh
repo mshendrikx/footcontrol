dac6a861c9348f2eb8ca9150457b020b61b57f2a
mshendrikx <84113289+mshendrikx@users.noreply.github.com>
2024-11-16 09:49:42 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-docker compose -f /home/mauricio/apps/docker/docker-compose.yml down footdraw
-docker rmi mservatius/footdraw
-docker build -t footdraw:latest .
-docker tag footdraw:latest mservatius/footdraw:latest
-docker push mservatius/footdraw:latest
-docker rmi footdraw:latest
-docker rmi mservatius/footdraw
-docker compose -f /home/mauricio/apps/docker/docker-compose.yml up footdraw -d
