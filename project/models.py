618d94dd63b05297b45fc4276955b5a25c673909
Mauricio Servatius Hendrikx <mauricio.servatius@gmail.com>
2024-11-18 08:31:32 -0300

    main

--- project/models.py
+++ project/models.py
@@ -1,20 +1,10 @@
 from flask_login import UserMixin
 from . import db
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    password = db.Column(db.String(1000))
-    email = db.Column(db.String(100))
-    phone = db.Column(db.Integer)   
-    admin = db.Column(db.Integer)  
-    groupid = db.Column(db.Integer)   
-    groupadm = db.Column(db.Integer)  
-
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    
+
 class Position(db.Model):
     groupid = db.Column(db.Integer, primary_key=True)
     posid = db.Column(db.Integer, primary_key=True)
@@ -25,6 +15,35 @@ class Game(db.Model):
     groupid = db.Column(db.Integer)
     date = db.Column(db.Biginteger)    
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(1000))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.Integer)   
+    admin = db.Column(db.Integer)  
+    groupid = db.Column(db.Integer)   
+    groupadm = db.Column(db.Integer)  
+
+class Player(db.Model):
+    groupid = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.String(1))
+    admin = db.Column(db.Integer)
+    
+class Skill(db.Model):
+    groupid = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.String(1))
+    value = db.Column(db.String(1))
+    
+class Vote(db.Model):
+    groupid = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, primary_key=True)
+    playerid = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.String(1))
+    value = db.Column(db.String(1))
+    
 class Attendance(db.Model):
     gameid = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, primary_key=True)
@@ -35,12 +54,9 @@ class Attendance(db.Model):
 class Team(db.Model):
     groupid = db.Column(db.Integer, primary_key=True)
     teamid = db.Column(db.Integer, primary_key=True)
-    
-class Player(db.Model):
     userid = db.Column(db.Integer, primary_key=True)
-    groupid = db.Column(db.Integer, primary_key=True)
-    position = db.Column(db.String(1))
-    admin = db.Column(db.Integer)
+    
+
 
 
     
